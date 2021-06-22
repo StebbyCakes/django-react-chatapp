0325dee9ae5fcebb00415766f1d673a08a4ee338
Esteban Carter <carterea1999@gmail.com>
2021-06-22 09:31:15 -0400

    Almost deployed

--- conf/settings.py
+++ conf/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_^)h$2_y9ejxt%e(0c#bd9&_48l6l(u^fvjw=!rw(w%$^ozqre'
-
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['full-stack-chatlog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
